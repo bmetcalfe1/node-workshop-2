fb00759bba2704b594c07566f55dea0abbc8dc3e
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-13 00:27:13 +0000

    changed syn to get rid of prompt but keep request

--- library/synonyms.js
+++ library/synonyms.js
@@ -1,49 +1,40 @@
 // Your API key is d4373215267d7efd5eb47347b40ebe81
 
-var prompt = require('prompt');
 var request = require('request');
 
-function SynonymAPI (apiKey) {
-  this.apiKey = apiKey;
-  this.getSynonyms = synonymsGetter;
+function SynonymAPI(apiKey) {
+    this.apiKey = apiKey;
 }
 
-module.exports = {
-    SynonymAPI: SynonymAPI
+SynonymAPI.prototype = {
+    getSynonyms: function(word, callback) {
+        var bhtCall = 'http://words.bighugelabs.com/api/2/' + this.apiKey + '/' + word + '/json';
+        request(bhtCall, function(err, wordResponse) {
+            if (err) {
+                callback(err);
+            }
+            else {
+                try {
+                    var jsonWordResponse = wordResponse.body;
+                    var jsonWordObj = JSON.parse(jsonWordResponse);
+                    callback(null, jsonWordObj);
+                }
+                catch (err) {
+                    callback(err);
+                }
+            }
+        });
+    }
 };
 
-function synonymsGetter() {
-    prompt.get("Word", function(err, word) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            var bhtCall ='http://words.bighugelabs.com/api/2/d4373215267d7efd5eb47347b40ebe81/' + word.Word + '/json';
-            // console.log(bhtCall); // for debugging
-        
-            request(bhtCall, function(err, wordResponse) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    var jsonWordResponse = wordResponse.body;
-                    //console.log(jsonWordResponse); //for debugging
-                    var jsonWordObj = JSON.parse(jsonWordResponse);
-                    console.log(jsonWordObj);
-                }
-            });
-        }
-    });
-}
-
-synonymsGetter();
+module.exports = SynonymAPI;
 
 // Attempt One
 // SynonymAPI.prototype = {
 //     getSynonyms: function(word, callback) {
-        
+
 //         var bhtCall ='http://words.bighugelabs.com/api/2/d4373215267d7efd5eb47347b40ebe81/' + word + '/json';
-        
+
 //         request(bhtCall, function(err, wordResponse) {
 //             if (err) {
 //                 console.log('there was an error');
@@ -62,9 +53,9 @@ synonymsGetter();
 
 // Attempt Two
 // var synonymsGetter = function(word, callback) {
-    
+
 //     var bhtCall ='http://words.bighugelabs.com/api/2/d4373215267d7efd5eb47347b40ebe81/' + word + '/json';
-    
+
 //     request(bhtCall, function(err, wordResponse) {
 //         if (err) {
 //             console.log('there was an error');
